4c71011b3d6294805d08fa33fa581511c1bbc83d
EstebanBennett <estebanbennettb@gmail.com>
2020-12-09 04:00:39 +0100

    Backend update

--- rocketBot/config/settings.py
+++ rocketBot/config/settings.py
@@ -2,4 +2,4 @@ import os
 
 PORT = int(os.environ.get('PORT', '8443'))
 HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME')
-PROJECT_SECRET_KEY = 'b6%6$2)rtmnwcy^imc+pgat)te(1n^vx+g$+zwn==v-zxim@q2'
+
--- rocketbotweb/api/urls.py
+++ rocketbotweb/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views.video import VideoView
+from .views import BotView, VideoView
 
 urlpatterns = [
     path('video/', VideoView.as_view(), name='video-view'),
+    path('bot/', BotView.as_view(), name='bot-view'),
 ]
